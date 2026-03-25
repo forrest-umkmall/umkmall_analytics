fa428c6f40965258f68d6475de6c40fcc64f75c9
forrest-umkmall <fhooton@umkmall.id>
2026-03-25 10:40:08 +0700

    Added user fields to int ai messages

--- dbt/models/intermediate/int_ai_chat_messages.sql
+++ dbt/models/intermediate/int_ai_chat_messages.sql
@@ -6,7 +6,7 @@
 }}
 
 /*
-Join AI chat messages with sessions to include user information.
+Join AI chat messages with sessions and users to include user information.
 This allows analysis of messages by user.
 */
 
@@ -16,12 +16,18 @@ with messages as (
 
 sessions as (
     select * from {{ ref('stg_ai_chat_sessions') }}
+),
+
+users as (
+    select * from {{ ref('stg_users') }}
 )
 
 select
     m.id as message_id,
     m.session_id,
     s.user_id,
+    u.email as user_email,
+    u.mobile as user_mobile,
     s.guest_session_id,
     s.title as session_title,
     m.role,
@@ -33,3 +39,4 @@ select
 
 from messages m
 left join sessions s on m.session_id = s.id
+left join users u on s.user_id = u.id
